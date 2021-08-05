fd2ef50aaf1d7b8df30c2d0ce5d0c1c237523014
elitandilashvili <elielitandilashvili@gmail.com>
2021-08-05 20:48:36 +0400

    add

--- mnscripts.js
+++ mnscripts.js
@@ -3,45 +3,45 @@
 // request.then((response)=>{
 //     console.log(response.data)
 // })
-function validateForm(){
+// function validateForm(){
 
+   
+// var pw1 = document.getElementById("password").Value;
+// var pw2 = document.getElementById("confirm_password").value; 
 
-var pw1 = document.getElementById("password").Value;
-var pw2 = document.getElementById("confirm_password").value; 
-
-if(pw1 =="") {
-    document.getElementById("message1").innerHTML = "**Fill the password please!";  
-     return false;  
-}
-if(pw2 =="") {
-    document.getElementById("message2").innerHTML = "**Fill the password please!";  
-     return false;  
-}
-if(pw1.length < 6) {  
-    document.getElementById("message1").innerHTML = "**Password length must be atleast 8 characters";  
-    return false;  
- }  
+// if(pw1 =="") {
+//     document.getElementById("message1").innerHTML = "**Fill the password please!";  
+//      return false;  
+// }
+// if(pw2 =="") {
+//     document.getElementById("message2").innerHTML = "**Fill the password please!";  
+//      return false;  
+// }
+// if(pw1.length < 6) {  
+//     document.getElementById("message1").innerHTML = "**Password length must be  6 characters";  
+//     return false;  
+//  }  
  
 
- if(pw1.length > 8) {  
-    document.getElementById("message1").innerHTML = "**Password length must not exceed 15 characters";  
-    return false;  
- } 
+//  if(pw1.length > 8) {  
+//     document.getElementById("message1").innerHTML = "**Password length must not exceed 15 characters";  
+//     return false;  
+//  } 
  
- else {  
-    alert("Password is correct");  
- }  
+//  else {  
+//     alert("Password is correct");  
+//  }  
 
- if(pw1 != pw2) {  
-    document.getElementById("message2").innerHTML = "**Passwords are not same";  
-    return false;  
-}
-else {  
-    alert ("Your password created successfully");  
-    document.write("JavaScript form has been submitted successfully");  
-  }  
+//  if(pw1 != pw2) {  
+//     document.getElementById("message2").innerHTML = "**Passwords are not same";  
+//     return false;  
+// }
+// else {  
+//     alert ("Your password created successfully");  
+    
+//   }  
 
-}
+// }
 
 
  
